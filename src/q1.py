dd043d8d4da3058eb84ac02815a747d7e87a51f5
Vks2024cdp <166885419+Vks2024cdp@users.noreply.github.com>
2024-12-11 13:13:05 +0800

    Update q1.py
    My answer

--- src/q1.py
+++ src/q1.py
@@ -6,10 +6,20 @@ def swap(x, y):
     - Return -1 if x and y is not numeric, and
     - print the swapped values if both x and y are numeric.
     """
+
+    if not (type(x) in [int, float] and type(y) in [int, float]):
+    return -1
+    x, y = y, x
+    print(f"Swapped values: x = {x}, y = {y}")
     return
 
 
 # Task 2
 # Invoke the function "swap" using the following scenarios:
 # - "Apple", 10
+output1 = swap("Apple", 10)  
+print("output:" , output1)
+
 # - 9, 17
+output2 = swap(9, 17) 
+print("output2:", output2)
